175eaa45aa7a4cbcaf670e0376d25420bd68149d
Moses <moses.junior@student.moringaschool.com>
2021-10-27 15:56:42 +0300

    adding a single page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
+import { SingleComponent } from './single/single.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingComponent
+    LandingComponent,
+    SingleComponent
   ],
   imports: [
     BrowserModule,
